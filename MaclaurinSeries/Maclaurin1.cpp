cd6e82923a580ddc908521238a68fa38ffe5fc7e
Yidan Yang <39606635+yidan-yang@users.noreply.github.com>
2020-09-13 22:42:16 -0400

    Update Maclaurin1.cpp

--- MaclaurinSeries/Maclaurin1.cpp
+++ MaclaurinSeries/Maclaurin1.cpp
@@ -7,3 +7,28 @@ Maclaurin series of f(x) = exp(2x)
 take advantage of the fact that (n+1)th term = nth term * 2*x/(n+1)
 
 */
+
+int main() {
+	int n = 100; // nums of iteration
+	int x = -10; //exponent value
+	double expval[120] = {}; //starting value = 1
+	double curTerm; // current term, where to start the simulation
+	double trueValue = exp(2 * x);
+	double rel_err; // relative error
+	double abs_err; // absolute error 
+	double tol = 1e-8; //tolerance
+
+
+	for (int i = 1; i <= n; i++) {
+		curTerm = ( 2 * x )/ i;
+		expval[i+1] = 1 + curTerm;
+		rel_err = abs(trueValue - expval[i+1]) / abs(trueValue);
+		abs_err = abs(trueValue - expval[i+1]);
+
+		if (expval[i+1] == expval[i]) {
+			break;
+		}
+		cout << "It needs " << i << " iterations to converge." << endl;
+	}
+	return 0;
+}
